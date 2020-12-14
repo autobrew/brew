4ee9df9ddb06abf634de90cb8e1836e3d642fd4f
Jeroen Ooms <jeroenooms@gmail.com>
2020-12-14 01:05:32 +0100

    Try older parallel to support Ruby 2.3

--- Library/Homebrew/style.rb
+++ Library/Homebrew/style.rb
@@ -17,6 +17,7 @@ module Homebrew
     def check_style_impl(files, output_type, options = {})
       fix = options[:fix]
 
+      Homebrew.install_gem! "parallel", "1.19.2"
       Homebrew.install_gem_setup_path! "rubocop", HOMEBREW_RUBOCOP_VERSION
       require "rubocop"
       require "rubocops"
